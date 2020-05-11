adf174468413369c5a0c34dc5a0ddae48c09c1b6
god <tomas.tyser@seznam.cz>
2020-05-11 05:41:57 -0700

    adding trip calc

---
+++ trip_calculator/trip_calculator.py
@@ -0,0 +1,8 @@
+kmto = int(input("Please enter the distance to you destination: "))
+ltper = float(input("Please enter liter per km: "))
+prsper = int(input("Please enter the price of fuel per liter: "))
+
+print()
+print("The cost of the trip will be", (kmto * ltper) * prsper)
+print()
+k=input("To exit press ENTER!")
